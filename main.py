dcb1744b3daf5f20a0a267a7cb2da8879dd255e7
isambarding <isambarding@gmail.com>
2018-05-01 15:51:52 +0100

    added main

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Container(BoxLayout):
+    pass
+
+
+class AssistantApp(App):
+    def build(self):
+        self.title = 'Assistant'
+        return Container()
+
+if __name__ == "__main__":
+    app = AssistantApp()
+    app.run()
