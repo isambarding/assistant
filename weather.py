052fc52202d125f758a09e75c84008b1b4b20f5f
isambarding <isambarding@gmail.com>
2018-05-03 09:26:02 +0100

    added all weather methods, tested working

--- weather.py
+++ weather.py
@@ -1,35 +1,96 @@
 import requests
 
+# 03/05/18: tested working
+# add function to retrieve days? easier that way?
 
-class Weather:
+# Uses 4 day request
+class Weather4Day:
     def __init__(self, country, city):
-        self.country = country
-        self.city = city
         url = "http://api.wunderground.com/api/034fa2c65c35e441/forecast/q/" + country + "/" + city + ".json"
         response = requests.get(url)
         self.data = response.json()
 
-    # Four day forecast
-    def fourdays(self):
+    # Today's forecast, in text
+    def forecastTodayText(self):
         s = ""
         for day in self.data["forecast"]["txt_forecast"]["forecastday"]:
-            s = s + day["title"] + ": " + day["fcttext_metric"] + "\n"
+            if day["period"] == 0:
+                s = day["fcttext_metric"]
         return (s)
 
-    # Tomorrow's forecast
-    def tomorrow(self):
+    # Today's highest forecast temperature, in celsius
+    def forecastTodayHigh(self):
+        s = ""
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            if day["period"] == 1:
+                s = day["high"]["celsius"]
+        return (s)
+
+    # Today's lowest forecast temperature, in celsius
+    def forecastTodayLow(self):
+        s = ""
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            if day["period"] == 1:
+                s = day["low"]["celsius"]
+        return (s)
+
+    # Tomorrow's forecast, text form
+    def forecastTomorrowText(self):
         s = ""
         for day in self.data["forecast"]["txt_forecast"]["forecastday"]:
             if day["period"] == 2:
-                s = s + day["title"] + ": " + day["fcttext_metric"] + "\n"
-            if day["period"] == 3:
-                s = s + day["title"] + ": " + day["fcttext_metric"] + "\n"
+                s = day["fcttext_metric"]
+        return (s)
+
+    # Tomorrow's highest forecast temperature, in celsius
+    def forecastTomorrowHigh(self):
+        s = ""
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            if day["period"] == 2:
+                s = day["high"]["celsius"]
+        return (s)
+
+    # Tomorrow's lowest forecast temperature, in celsius
+    def forecastTomorrowLow(self):
+        s = ""
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            if day["period"] == 2:
+                s = day["low"]["celsius"]
+        return (s)
+
+# Uses 10 day request
+class Weather10Day:
+    def __init__(self, country, city):
+        url = "http://api.wunderground.com/api/034fa2c65c35e441/forecast10day/q/" + country + "/" + city + ".json"
+        response = requests.get(url)
+        self.data = response.json()
+
+    # 10 day forecast, text version
+    def forecast10DaysText(self):
+        s = []
+        for day in self.data["forecast"]["txt_forecast"]["forecastday"]:
+            if day["period"] % 2 == 0:
+                s.append(day["fcttext_metric"])
+        return (s)
+
+    # Highest forecast temperature of next 10 days, in celsius
+    def forecast10DaysHigh(self):
+        s = []
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            s.append(day["high"]["celsius"])
+        return (s)
+
+    # Lowest forecast temperature of next 10 days, in celsius
+    def forecast10DaysLow(self):
+        s = []
+        for day in self.data["forecast"]["simpleforecast"]["forecastday"]:
+            s.append(day["low"]["celsius"])
         return (s)
 
 
 # testing
-country = input("Enter country: ")
-city = input("Enter city: ")
-w = Weather(country, city)
-print(w.tomorrow())
+country = "france"  # input("Enter country: ")
+city = "paris"  # input("Enter city: ")
+w = Weather10Day(country, city)
+print(w.forecast10DaysLow())
 
