fcefa130af12039a766f597d2747cff78cc30ef1
isambarding <isambarding@gmail.com>
2018-05-04 14:02:35 +0100

    fixed last 10 tweet function

--- twitter.py
+++ twitter.py
@@ -1,6 +1,8 @@
 import requests
 from requests_oauthlib import OAuth1
 
+# Class hold all methods related to the Twitter section
+# Note that the API does not support Twitter's new 280 character tweets, so they are shortened.
 class Twitter:
     def __init__(self):
         token = "989416304108064770-HwtBl1mm13xiKBOMqlTjq2roq9SZCh0"
@@ -17,19 +19,18 @@ class Twitter:
         q = f[0]
         return(q["text"])
 
-    # not working
+    # Gets the previous 10 tweets from a given username.
     def user10(self, username):
         url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + username + "&count=10"
         response = requests.get(url, auth=self.auth)
         f = response.json()
-        q = f[0]
         s=[]
-        for tweet in q:
-            s.append(q["text"])
+        for tweet in f:
+            s.append(tweet["text"])
         return(s)
 
 
 #Testing
 t = Twitter()
 un = input("Enter an @username: ")
-print(t.userLatest(un))
+print(t.user10(un))
