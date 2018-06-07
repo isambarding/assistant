9b52ccbbf3f3da2bf957c51032db015d58203ef8
isambarding <isambarding@gmail.com>
2018-06-07 10:52:55 +0100

    started settings

--- settings.py
+++ settings.py
@@ -1,1 +1,14 @@
 # settings file: user changes name and location details
+import sqlite3
+
+class Settings:
+    def changeName(self, name):
+        with sqlite3.connect("UserData.db") as db:
+            cursor = db.cursor()
+            cursor.execute('UPDATE userInfo SET Name="' + name + '"')
+
+    def changeLocation(self, country, city):
+        with sqlite3.connect("UserData.db") as db:
+            cursor = db.cursor()
+            cursor.execute('UPDATE userInfo SET Country="' + country + '"')
+            cursor.execute('UPDATE userInfo SET City="' + city + '"')
